fd7487f390af9675fa1e684f91d6e5a54c3fe1b1
Pavel <93088269+KhaninPasha0488@users.noreply.github.com>
2022-09-04 15:25:26 +0300

    create components Tag, create Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,31 @@
 import type { NextPage } from 'next'
-import {Button, Htag, P} from "../components";
+import {Button, Htag, P, Rating, Tag} from "../components";
+import { useState} from "react";
+import {withLayout} from "../layaut/Layout";
 
-const Home: NextPage = () => {
+
+ function Home() :JSX.Element {
+const [rating,setRating] =useState<number>(4)
   return (
    <>
-       <Htag tag="h1">Текст</Htag>
+       <Htag tag="h1">Заголовок</Htag>
        <Htag tag="h2">Текст</Htag>
        <Htag tag="h3">Текст</Htag>
-       <Button appearance="primary" arrow='right'>Кнопка</Button>
+       <Button appearance="primary" arrow='right' >Кнопка</Button>
        <Button appearance="ghost" arrow='down'>Кнопка</Button>
        <P size='s'>Маленький</P>
        <P >Средний</P>
        <P size='l' >Большой</P>
+       <Tag color="red" size='m'>Red</Tag>
+       <Tag color="green" size='m'>Green</Tag>
+       <Tag color="primary" size='m'>Primary</Tag>
+       <Tag color="gray">Gray</Tag>
+       <Tag color="ghost" >Ghost</Tag>
+        <Rating rating={rating} isEditable setRating={setRating} />
    </>
 
   )
 }
 
-export default Home
+
+export default withLayout(Home);
